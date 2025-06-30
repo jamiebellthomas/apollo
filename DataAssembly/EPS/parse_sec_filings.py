5c7270a7f3f9e0d53ae1ea5c11f5ade6708e1ede
jamie <jamiebt@live.co.uk>
2025-06-30 23:36:56 +0100

    Finished first pass at EPS data

--- DataAssembly/EPS/parse_sec_filings.py
+++ DataAssembly/EPS/parse_sec_filings.py
@@ -430,7 +430,7 @@ def main(start:int):
         print("[INFO] All rows processed successfully.")
 
 if __name__ == "__main__":
-    main(start=6000)
+    main(start=7640)
     # data = (extract_relevant_eps_data_html("https://www.sec.gov/Archives/edgar/data/820313/000155837024013696/aph-20240930x10q.htm"))
     # for i in data:
     #     print("-------------------")
