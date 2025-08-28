fcace9447f7f826ab5bbed89d8bc6085f5f6ee07
jamie <jamiebt@live.co.uk>
2025-08-28 13:25:35 +0100

    Subgraph analysis plots

--- KG/SubGraph.py
+++ KG/SubGraph.py
@@ -1139,7 +1139,7 @@ def load_first_with_exact_facts(jsonl_path: str, n: int = 5) -> dict:
 if __name__ == "__main__":
     # ---------- test ----------
     # 1) pick a subgraph with exactly 5 facts
-    obj = load_first_with_exact_facts(config.SUBGRAPHS_JSONL, n=85)
+    obj = load_first_with_exact_facts(config.SUBGRAPHS_JSONL, n=125)
     sg = SubGraph(**obj)
     # sg.visualise_pricing()  # Visualise the pricing data for the subgraph
 
